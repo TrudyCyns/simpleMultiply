ce637c67918b46313b5b92aca5f441165fa1cbea
TrudyCyns <gcyn51a@gmail.com>
2022-09-04 19:32:29 +0300

    Test First TDD Cycle 2.1

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const multiply = require('../multiply');
 
 describe('Multiplication', () => {
   it('Multiplication of 1 test', () => {
-    assert.equal(multiply(1, 1), 1);
+    assert.equal(multiply(2, 2), 4);
   });
 });
